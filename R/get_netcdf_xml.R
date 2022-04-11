753ca3b5d68d260de4fd1c57796eed02b93e331a
kimberly-bastille <kimberly.bastille@gmail.com>
2022-04-11 16:45:16 -0400

    add csv and xml examples from inportXML

--- R/get_netcdf_xml.R
+++ R/get_netcdf_xml.R
@@ -1,1 +1,15 @@
-## Function to scrape metadata to build XML
+## Function to scrape netcdf metadata to build XML
+library(tidyverse)
+
+nc <- ncdf4::nc_open(file.path(here::here("data/sst.day.mean.2021.nc"))) #netcdf example
+
+example_csv<- read.csv(file.path(here::here("data-raw/InportXML-template.csv"))) #csv example
+
+# only required fields
+req_example<- example_csv %>%
+  dplyr::filter(value == "REQUIRED")
+
+# Example metadata extraction
+title<- nc$var$sst$longname
+accronym<- nc$var$sst$name
+dimension_count<- nc$var$sst$ndims
